548965af52a1c28708ce413ad606f20518cf284b
fuad manye / <fahadbasi333@gmail.com>
2022-06-12 14:23:20 +0300

    create urls.py in apis folder

---
+++ apis/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.get_routes, name='routes'),
+    path('get-projects/', views.get_projects, name='projects'),
+    path('get-project/<int:pk>/', views.get_project, name="project"),
+    path('profile/<int:pk>/',views.profile, name='profile_edit-profile'),
+    path('register/', views.authentication, name='registration'),
+    path('users/', views.get_users, name='users')
+]
