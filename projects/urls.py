7c48a9f32c737267b0d9522b7204dcabed374efe
fuad manye / <fahadbasi333@gmail.com>
2022-06-12 14:19:28 +0300

    create urls.py in projects

---
+++ projects/urls.py
@@ -0,0 +1,10 @@
+from  django.urls import path 
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('posts/', views.posts, name='posts'),
+    path('create-post/', views.create_post, name='create-post'),
+    path('like-post/', views.like_post, name='like'),
+    path('submit-review/<int:pk>/', views.submit_review ,name='ratings')
+]
